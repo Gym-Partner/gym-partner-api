0b53567c3fd688d73b257c2427efbf4a606b7866
Titouan Escorneboueu <t.escorneboueu@gmail.com>
2024-10-07 14:12:10 +0200

    Start writing User's usecase

--- domain/model/user.model.go
+++ domain/model/user.model.go
@@ -1,2 +1,12 @@
 package model
 
+type User struct {
+    Id string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	UserName string `json:"user_name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Users []User
--- usecases/repository/user/user.repository.go
+++ usecases/repository/user/user.repository.go
@@ -1,2 +1,14 @@
 package user
 
+import "gitlab.com/gym-partner1/api/gym-partner-api/domain/model"
+
+type IUserRepository interface {
+    IsExist(data, OPT string) bool
+
+	GetAll() (model.Users, error)
+    GetOneById(uid string) (model.User, error)
+
+    Create(data model.User) error
+    Update(data model.User) error
+    Delete(uid string) error
+}
