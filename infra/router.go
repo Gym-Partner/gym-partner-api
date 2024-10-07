641d46d1e8baa8503b53c701f64b6d959351948c
Titouan Escorneboueu <t.escorneboueu@gmail.com>
2024-10-07 14:32:25 +0200

    Add router

--- infra/router.go
+++ infra/router.go
@@ -1,2 +1,18 @@
 package infra
 
+import (
+    "gitlab.com/Titouan-Esc/api_common/logger"
+    "gitlab.com/Titouan-Esc/api_common/mongo"
+    "gitlab.com/Titouan-Esc/api_common/router"
+    "gitlab.com/gym-partner1/api/gym-partner-api/interfaces/controller"
+)
+
+func Dispatch(sql *mongo.Mongo, logServ, logSess *logger.Log) *router.Router {
+	route := router.NewRouter()
+
+	userController := controller.NewUserController(sql, logSess)
+
+	route.AddCORS()
+
+	return route
+}
--- main.go
+++ main.go
@@ -1,2 +1,2 @@
-package gym_partner_api
+package main
 
